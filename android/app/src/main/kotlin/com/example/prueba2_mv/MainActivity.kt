649ae86216c375e6d0987a9168d43e71337ac5fa
Renzo Martin Rosas Vidal <renzo.rosas@servexternos.grupoavalon.es>
2022-09-01 20:14:32 -0500

    first commit and homecscreem

---
+++ android/app/src/main/kotlin/com/example/prueba2_mv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prueba2_mv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
+          ),
+          body: HomeScreen()),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
